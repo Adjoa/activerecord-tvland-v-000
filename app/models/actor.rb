bb03bbe5808c3a2af8ef496ac1629260eca693bf
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-18 02:28:18 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.each do |character|
-      [character.name, character.show].join(' - ')
+    self.characters.collect do |character|
+      [character.name, Show.find_by(id: character.show_id).name].join(' - ')
     end
   end
 end
